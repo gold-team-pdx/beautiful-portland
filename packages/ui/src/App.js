cb94ffa47fef91adf0543cf146e3d43ab5caa22f
Eduardo Robles <robles5@pdx.edu>
2019-02-24 12:34:10 -0800

    Added public folder & index

---
+++ packages/ui/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>Beautiful Portland</h1>
+				<p>Welcome to our site</p>
+			</div>
+		);
+	}
+}
+
+export default App;
