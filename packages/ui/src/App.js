ea94bbaa6570660320e35720f3fd30a73e6371ed
David Gilmore <dwg2@pdx.edu>
2019-03-05 19:22:22 -0800

    finished sticky footer

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Footer from "./components/Footer";
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
+			<div className="App" style={{ display:"flex", minHeight:"100vh", flexDirection:"column" }}>
 				<h1>Beautiful Portland</h1>
 				<p>Welcome to our site</p>
+				<div style={{ flex:1 }}></div>
+				<Footer />
 			</div>
 		);
 	}
