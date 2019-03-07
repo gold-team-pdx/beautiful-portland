fa541d31b3368f230aa8e7af37a5efe55fa0e088
Ronnie Song <ronsong@pdx.edu>
2019-03-06 23:17:16 -0800

    feat(ui): POST request to the api
    use the submitted content to make a POST to the api by axios

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import VolunteerForm from './components/VolunteerForm';
+import { Container } from 'semantic-ui-react';
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<h1>Beautiful Portland</h1>
 				<p>Welcome to our site</p>
+				<Container>
+					<VolunteerForm />
+				</Container>
 			</div>
 		);
 	}
--- packages/ui/src/components/VolunteerForm.js
+++ packages/ui/src/components/VolunteerForm.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import Item from './Item';
 import { Header } from 'semantic-ui-react';
+import Axios from 'axios';
 
 export default class VolunteerForm extends Component {
 	onSubmit = (data) => {
 		console.log(JSON.stringify(data, null, 3));
+		Axios.post('/api/form', {
+			body: data,
+			header: {		
+				'Content-Type': 'application/json'
+			}
+		}).then(response => {
+			console.log(response, 'Form Submitted')
+		}).catch(err => {
+			console.log(err, 'Try again.')
+		})
 	};
 	render() {
 		return (
