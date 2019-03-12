e18dad6e816c461ecedf6cf8834f0a29e670f618
babileej <bailee@pdx.edu>
2019-03-11 21:59:26 -0700

    added default images to page

--- packages/ui/src/Components/Slider.js
+++ packages/ui/src/Components/Slider.js
@@ -3,11 +3,23 @@ import Slide from './Slide'
 import Axios from 'axios'
 import './Stylesheets/Slider.css'
 
+const defaultImages = [        
+    "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/aurora.jpg",
+    "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/canyon.jpg",
+    "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/city.jpg",
+    "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/desert.jpg",
+    "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/mountains.jpg",
+    "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/redsky.jpg",
+    "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/sandy-shores.jpg",
+    "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/tree-of-life.jpg"
+]
+
 export default class Slider extends Component {
     constructor(props) {
       super(props)
       this.state = {
-        // Current Images come from a public S3 container
+        // Current Images come from a public S3 container. If any images 
+        // are in minio instance, they will be served instead.
         images: [],
         currentImageIndex: 0,
         // Timer for transitions
@@ -29,7 +41,12 @@ export default class Slider extends Component {
             // handle error
             console.log(err)
         })
-        urls = storage.getItem('urls').toString().split(',')
+        if(storage.length > 0) {
+            urls = storage.getItem('urls').toString().split(',')
+        }
+        else {
+            urls = defaultImages
+        }
         this.setState({
             images: urls,
             timer: timer
