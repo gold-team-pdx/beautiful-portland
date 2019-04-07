da2a4daf0e83d9490886ad9ca05d9e55b9854f25
Eduardo Robles <robles5@pdx.edu>
2019-04-06 21:02:13 -0700

    Added context api

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Home from './Components/Home'
-import VolunteerForm from './Components/VolunteerForm'
+import React, { Component } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import MyProvider from "./Components/MyProvider"
+import Home from "./Components/Home"
+import VolunteerForm from "./Components/VolunteerForm"
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<div className="App">
-					<Route exact path="/" component={Home} />
-					<Route path="/volunteer-form" component={VolunteerForm} />
-				</div>
-			</Router>
-		)
-	}
+  render() {
+    return (
+      <MyProvider>
+        <Router>
+          <div className="App">
+            <Route exact path="/" component={Home} />
+            <Route path="/volunteer-form" component={VolunteerForm} />
+          </div>
+        </Router>
+      </MyProvider>
+    )
+  }
 }
 
 export default App
