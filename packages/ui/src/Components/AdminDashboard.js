adfce8d3bef734154f4a8d9525ecbc5a53d8be1e
babileej <bailee@pdx.edu>
2019-04-09 19:29:50 -0700

    minor changes to AdminDashboard.js to make menu with submenus

--- packages/ui/src/Components/AdminDashboard.js
+++ packages/ui/src/Components/AdminDashboard.js
@@ -6,7 +6,7 @@ import './Stylesheets/AdminDashboard.css'
 export default class AdminDashboard extends Component {
     state = {
         // Default active content
-        activeItem: 'volunteerList'
+        activeItem: 'viewEvents'
     }
 
     handleItemClick = (e, { name }) => this.setState({activeItem: name})
@@ -18,14 +18,14 @@ export default class AdminDashboard extends Component {
             <div className='adminDash'>
                     <Menu size='huge' inverted color='teal'>
                         <Menu.Item>
-                            Logo Here? or Menu here?
+                            Logo Here?
                         </Menu.Item>
                         <Menu.Menu position='right'>
                             <Menu.Item>
                                 {/* We could change this to be whichever user is logged in */}
                                 Hi Jenn!
                             </Menu.Item>
-                            <Menu.Item>
+                            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick}>
                                 Logout
                             </Menu.Item>
                         </Menu.Menu>
@@ -33,18 +33,42 @@ export default class AdminDashboard extends Component {
                 <div className='adminView'>
                     <div className='dashMenu'>
                         <Menu vertical size='huge'>
-                            <Menu.Item name='events' active={activeItem === 'events'} onClick={this.handleItemClick}>
+                            <Menu.Item name='events'>
                                 Events
                             </Menu.Item>
+                            <Menu.Menu>
+                                <Menu.Item name='addEvent' active={activeItem === 'addEvent'} onClick={this.handleItemClick}>
+                                    Add Event
+                                </Menu.Item>
+                                <Menu.Item name='viewEvents' active={activeItem === 'viewEvents'} onClick={this.handleItemClick}>
+                                    View Upcoming Events
+                                </Menu.Item>
+                            </Menu.Menu>
                             <Menu.Item name='volunteerList' active={activeItem === 'volunteerList'} onClick={this.handleItemClick}>
-                                Volunteers
+                                Volunteer List
                             </Menu.Item>
-                            <Menu.Item name='addImages' active={activeItem === 'addImages'} onClick={this.handleItemClick}>
-                                Add Image
+                            <Menu.Item name='Images'>
+                                Images
                             </Menu.Item>
-                            <Menu.Item name='newStory' active={activeItem === 'newStory'} onClick={this.handleItemClick}>
-                                New Story
+                            <Menu.Menu>
+                                <Menu.Item name='addImages' active={activeItem === 'addImages'} onClick={this.handleItemClick}>
+                                    Add Image
+                                </Menu.Item>
+                                <Menu.Item name='removeImages' active={activeItem === 'removeImages'} onClick={this.handleItemClick}>
+                                    Remove Image
+                                </Menu.Item>
+                            </Menu.Menu>
+                            <Menu.Item name='stories'>
+                                Stories
                             </Menu.Item>
+                            <Menu.Menu>
+                                <Menu.Item name='createStory' active={activeItem === 'createStory'} onClick={this.handleItemClick}>
+                                    Create Story
+                                </Menu.Item>
+                                <Menu.Item name='removeStory' active={activeItem === 'removeStory'} onClick={this.handleItemClick}>
+                                    Remove Story
+                                </Menu.Item>
+                            </Menu.Menu>
                         </Menu>
                     </div>
                     {/* Changed these to divs so we can work some CSS magic on them */}
