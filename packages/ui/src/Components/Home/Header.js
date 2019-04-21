2c6587eec38acbb078c6c42d884e8335aa5c0ef1
ronniesong0809 <ronniesong0809@gmail.com>
2019-04-21 13:28:21 -0700

    make the logo as part of the Menu, and treat it as NavLink redirect the home page once clicked it

--- packages/ui/src/Components/Home/Header.js
+++ packages/ui/src/Components/Home/Header.js
@@ -11,74 +11,64 @@ export default class Header extends Component {
 		const { activeItem } = this.state
 
 		return (
-			<div>
-				<div style={{width: '33%', float:"left"}}>
-					<Menu
-						inverted
-						color = {"olive"}
-						styles={{borderRadius:'100px'}}
-						size = "massive"
-						widths={2}
-						style={{
-							flexShrink: 100000, //don't allow flexbox to shrink it
-							borderRadius: 10, //clear semantic-ui style
-							margin: 0 //clear semantic-ui style
-						}}
-					>
-						<Menu.Item
-							name="About"
-							as={NavLink}
-							active={activeItem === 'About'}
-							onClick={this.handleItemClick}
-							to="/about"
-						>
-							About
-						</Menu.Item>
-						<Menu.Item
-							name="Volunteer Calender"
-							as={NavLink}
-							active={activeItem === 'Volunteer Calender'}
-							onClick={this.handleItemClick}
-							to="/volunteer-form"
-						>
-							Volunteer Calender
-						</Menu.Item>
-					</Menu>
-				</div>
+			<Menu
+				inverted
+				color = {"#4286f4"}
+				size = "massive"
+				widths={5}
+				style={{
+					flexShrink: 100000, //don't allow flexbox to shrink it
+					borderRadius: 0, //clear semantic-ui style
+					margin: 0 //clear semantic-ui style
+				}}
+			>
+				<Menu.Item
+					name="About"
+					as={NavLink}
+					active={activeItem === 'About'}
+					onClick={this.handleItemClick}
+					to="/about"
+				>
+					About
+				</Menu.Item>
+				<Menu.Item
+					name="Volunteer Calender"
+					as={NavLink}
+					active={activeItem === 'Volunteer Calender'}
+					onClick={this.handleItemClick}
+					to="/volunteer-form"
+				>
+					Volunteer Calender
+				</Menu.Item>
 
-				<div style={{width: '33%', float:"right"}}>
-					<Menu
-						inverted
-						color = {"olive"}
-						size = "massive"
-						widths={2}
-						style={{
-							flexShrink: 100000, //don't allow flexbox to shrink it
-							borderRadius: 10, //clear semantic-ui style
-							margin: 0 //clear semantic-ui style
-						}}
-					>
-						<Menu.Item
-							name="Stories"
-							as={NavLink}
-							active={activeItem === 'Stories'}
-							onClick={this.handleItemClick}
-							to="/Stories"
-						>
-							Stories
-						</Menu.Item>
-						<Menu.Item
-							name="Contact Us"
-							as={NavLink}
-							active={activeItem === 'Contact Us'}
-							onClick={this.handleItemClick}
-							to="/ContactUs"
-						>
-							Contact Us
-						</Menu.Item>
-					</Menu>
-				</div>
-			</div>
+				<Menu.Item
+					active={activeItem === 'Logo'}
+					as={NavLink}
+					onClick={this.handleItemClick}
+					to="/"
+				>
+				<img src='/logo-white.png' />
+				</Menu.Item>
+
+				<Menu.Item
+					name="Stories"
+					as={NavLink}
+					active={activeItem === 'Stories'}
+					onClick={this.handleItemClick}
+					to="/Stories"
+				 >
+					Stories
+				</Menu.Item>
+				<Menu.Item
+					name="Contact Us"
+					as={NavLink}
+					active={activeItem === 'Contact Us'}
+					onClick={this.handleItemClick}
+					to="/ContactUs"
+				>
+					Contact Us
+				</Menu.Item>
+			</Menu>
 		)
 	}
 }
