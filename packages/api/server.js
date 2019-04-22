1d7b619f0e23c70a4d299c9f1f5b27c177867334
DJ Gilmore-Innis <dwg2@pdx.edu>
2019-04-21 21:20:48 -0700

    Update server.js

--- packages/api/server.js
+++ packages/api/server.js
@@ -103,6 +103,11 @@ app.get('/api/logout', function(req, res) {
   res.redirect(`${process.env.UI_SERVER}/`)
 })
 
+// Visitor request handlers
+app.get('/api/getImages', visitorHandlers.homeImages.bind({amazon: AWS}))
+app.get('/api/event', visitorHandlers.volunteerFormGetEventInfo.bind({dbClient: client}))
+app.post('/api/form', visitorHandlers.volunteerFormSubmit.bind({dbClient: client}))
+
 
 // Admin request handlers
 app.get('/api/admin-dashboard', ensureAuthenticated, function(req, res) {
