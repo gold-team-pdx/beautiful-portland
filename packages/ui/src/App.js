42672fb99c37bc68aabef0677e9ea0ac0cf968f7
mick <mccoyc@cat.pdx.edu>
2019-05-05 13:29:22 -0700

    feat(ui): cleanup some bad autoformatting

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import MyProvider from "./Components/Context/MyProvider"
-import Home from "./Components/Home/Home"
-import About from "./Components/About/About"
-import VolunteerForm from "./Components/Volunteer/VolunteerForm"
-import Login from "./Components/Dashboard/Login"
-import AdminDashboard from "./Components/Dashboard/AdminDashboard"
-import ContactUs from "./Components/Contact/ContactUs.js"
-import NoMatch from "./Components/Error/NoMatch"
-import "./App.css"
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import MyProvider from './Components/Context/MyProvider'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import VolunteerForm from './Components/Volunteer/VolunteerForm'
+import Login from './Components/Dashboard/Login'
+import AdminDashboard from './Components/Dashboard/AdminDashboard'
+import ContactUs from './Components/Contact/ContactUs.js'
+import NoMatch from './Components/Error/NoMatch'
+import './App.css'
 
 class App extends Component {
   render() {
